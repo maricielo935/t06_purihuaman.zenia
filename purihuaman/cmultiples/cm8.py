a4447a07dfec168661fd91ee15a37d693951206e
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-17 15:04:28 -0500

    Se agrego el 8to ejercicio de condicional multiple

---
+++ purihuaman/cmultiples/cm8.py
@@ -0,0 +1,35 @@
+import os
+#PREMIO POR COMPRAS EN PLAZA VEA
+#Declaracion de variables
+cliente,producto,cantidad,precio_producto="","",0,0.0
+
+#INPUT
+cliente=os.sys.argv[1]
+producto=os.sys.argv[2]
+cantidad=int(os.sys.argv[3])
+precio_producto=float(os.sys.argv[4])
+
+#PROCESSING
+monto=(cantidad*precio_producto)
+
+#VERIFICADOR
+monto=(monto>50)
+
+#OUTPUT
+print("###################################")
+print("# PREMIO POR COMPRAS EN PLAZA VEA #")
+print("###################################")
+print(" # Cliente          :", cliente)
+print(" # Producto         :", producto)
+print(" # Precio por unidad:", precio_producto,"soles")
+print(" # Cantidad         :", cantidad,"unidades")
+print("###################################")
+
+#Condicion multiple
+#SI el monto resulta mayor a 5o soles , mostrarle Gano un descuento del 50% en Hamburgesas BEMBOS
+if ( monto==True):
+    print("Gano un descuento del 50% en Hamburgesas BEMBOS.")
+#fin_if
+if ( monto<50):
+    prin("Gano 5 puntos bonus.")
+#fin_if
