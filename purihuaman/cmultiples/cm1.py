e56b38f7d7b804f86c7adb53542f97771d8c6ed1
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-17 14:27:50 -0500

    Se agrego el 1er ejercicio de condicional multiple

---
+++ purihuaman/cmultiples/cm1.py
@@ -0,0 +1,37 @@
+import os
+#BOLETA DE NOTAS
+#declaracion de variables
+nota1,nota2,nota3,nota4=0,0,0,0
+
+#INPUT
+nota1=int(os.sys.argv[1])
+nota2=int(os.sys.argv[2])
+nota3=int(os.sys.argv[3])
+nota4=int(os.sys.argv[4])
+
+#PROCESSING
+promedio_final=(nota1+nota2+nota3+nota4)/4
+
+#VERIFICADOR
+aprobar=(promedio_final>=11)
+
+#OUTPUT
+print("#####################################")
+print("#  BOLETA   DE   NOTAS  DE  FISICA  #")
+print("#####################################")
+print("  # Nota 1        :  ", nota1)
+print("  # Nota 2        :  ", nota2)
+print("  # Nota 3        :  ", nota3)
+print("  # Nota 4        :  ", nota4)
+print("  #Promedio final :  ", promedio_final)
+print("#####################################")
+
+#Condicion multiple
+#SI el promedio es mayor o igual 11, mostrarle Aprobado
+if ( aprobar>=11):
+    print("Curso Aprobado.")
+#fin_if
+if ( aprobar<11):
+    print("Curso Desaprobado.")
+#fin_if
+
