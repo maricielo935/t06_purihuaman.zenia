6403243b82ccffaf2ceea216e137a6e51a2498b8
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-17 14:42:33 -0500

    Se agrego el 10mo ejercicio de condicional multiple

---
+++ purihuaman/cmultiples/cm10.py
@@ -0,0 +1,30 @@
+import os
+#PREMIO POR NUMERO DE ETIQUETAS DE LECHE GLORIA
+#Declaracion de variables
+nombre,apellidos,nro_etiquetas="","",0
+
+#INPUT
+nombre=os.sys.argv[1]
+apellidos=os.sys.argv[2]
+nro_etiquetas=int(os.sys.argv[3])
+
+#VERIFICADOR
+reclamar_premio=(nro_etiquetas>10)
+
+#OUTPUT
+print("############################################")
+print("#PREMIO POR N° DE ETIQUETAS DE LECHE GLORIA#")
+print("############################################")
+print(" # Nombre        :", nombre)
+print(" # Apellidos     :", apellidos)
+print(" # N°de etiquetas:", nro_etiquetas)
+print("############################################")
+
+#Condicion multiple
+#SI la persona logro juntar mas de 10 etiquetas , mostrarle RECLAMAR 3 TARROS DE LECHE GLORIA GRATIS
+if ( reclamar_premio==True):
+    print("RECLAMAR 3 TARROS DE LECHE GLORIA GRATIS.")
+#fin_if
+if ( reclamar_premio==False):
+    print("Seguir intentando")
+#fin_if
