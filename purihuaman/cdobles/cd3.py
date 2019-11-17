cdc4e07595e9c85a8d6852e5ee0aa75a47d6f6bc
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-17 14:06:14 -0500

    Se agrego el 3er ejercicio de condicional doble

---
+++ purihuaman/cdobles/cd3.py
@@ -0,0 +1,38 @@
+import os
+#BOLETA DE LA BOTICA SBCH
+#declaracion de variables
+cliente,producto,laboratorio,cantidad,precio="","","",0,0.0
+
+#INPUT
+cliente=os.sys.argv[1]
+producto=os.sys.argv[2]
+laboratorio=os.sys.argv[3]
+cantidad=int(os.sys.argv[4])
+precio=float(os.sys.argv[5])
+
+#PROCESSING
+monto_final=(precio*cantidad)
+
+#VERIFICADOR
+puntos_bonus=(monto_final>40)
+
+#OUTPUT
+print("##############################################")
+print("#BOTICA SOCIEDAD DE BENEFICIENCIA DE CHICLAYO#")
+print("##############################################")
+print(" #  Cliente       :  ",  cliente)
+print(" #  Producto      :  ",  producto)
+print(" #  Laboratorio   :  ",  laboratorio)
+print(" #  Cantidad      :  ",  cantidad)
+print(" #  Precio        :  ",  precio)
+print(" #  Subtotal      :  ",  monto_final)
+print("##############################################")
+
+#Condicion doble
+#SI el monto final es mayor a 40, mostrarle Gano 10 puntos bonus
+if ( puntos_bonus==True):
+    print("Gano 10 puntos Bonus, que puede utilizar en su proxima compra.")
+#fin_if
+else:
+    print("Vuelva pronto.")
+#fin_else
