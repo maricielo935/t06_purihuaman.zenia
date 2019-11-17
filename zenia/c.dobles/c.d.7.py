e59b99035138cef5bb9aebdd2c359e24b0051c8d
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-17 15:28:38 -0500

    Se agrego el septimo ejercicio de condicional doble

---
+++ zenia/c.dobles/c.d.7.py
@@ -0,0 +1,33 @@
+import os
+#INPUT
+cliente=os.sys.argv[1]
+cajero=os.sys.argv[2]
+kg_de_manzanas=int(os.sys.argv[3])
+precio_por_cada_kg_de_manzana=float(os.sys.argv[4])
+
+#PROCESSING
+pago_total=(kg_de_manzanas*precio_por_cada_kg_de_manzana)
+
+#VALIDADOR
+validador_del_pago_total=(pago_total==16)
+
+#OUTPUT
+print("#############################################################################################")
+print("################# BOLETA DE COMPRA ##########################################################")
+print("########### cliente:" , cliente , "##########################################################")
+print("########### cajero:" , cajero , "############################################################")
+print("########### kd de manzanas", kg_de_manzanas , "##############################################")
+print("########### precio por cada kg de manzana: s/.", precio_por_cada_kg_de_manzana, "############")
+print("#############################################################################################")
+print("###### pago total:", pago_total , "##########################################################")
+print("#############################################################################################")
+
+#CONDICION DOBLE
+#si el comprador es moderado mostrar un premio al pagar
+if (comprador_moderado==True):
+    print("FELICIDADES")
+
+else:
+    print("CONTROLECE AL COMPRAR")
+
+#fin_if
